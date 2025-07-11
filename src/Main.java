659ef4903015b804f098bc205f74ceffb69f908c
Luis9768 <luiskasami@gmail.com>
2025-07-11 13:03:43 -0300

    primeiro commit aprendizagem

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String [] args){
+        //Variaveis do JAVA
+        byte => 8 bits =>-128 a 127
+            short => 16 bits => -32.768 a 32.767
+            int => 32 bits =>
+    }
+
+
+}
